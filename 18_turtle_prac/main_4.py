5a9dedf74f1477c2d1ae31e8d0fec2c70e2d8ac0
Disa <dias.issabek.di@gmail.com>
2022-08-06 21:12:29 +0600

    Turtle: spirograph

---
+++ 18_turtle_prac/main_4.py
@@ -0,0 +1,29 @@
+import turtle
+import time
+import random
+
+tim = turtle.Turtle()
+
+turtle.colormode(255)
+tim.speed(0)
+directions = [0, 90, 180, 270]
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+def draw_spirogpraph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirogpraph(5)
+
+screen = turtle.Screen()
+screen.exitonclick()
